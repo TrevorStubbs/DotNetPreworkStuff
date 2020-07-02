3ce8b6b298ba5ded74ff5ccd00187c0334880884
Trevor Stubbs <stubbste@gmail.com>
2020-07-02 14:38:16 -0700

    Add project files.

---
+++ DotNetPreworkStuff/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DotNetPreworkStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // control k+d to format
+            // program class and the main method are the insertion points for the program
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
